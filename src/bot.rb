a3490f3516aed0b2c83250d1bf33dc410bb70074
Drew Martin <drew.martin@shopify.com>
2017-02-13 21:55:56 -0500

    Add invite url

--- src/bot.rb
+++ src/bot.rb
@@ -3,6 +3,8 @@ require 'discordrb'
 token = ENV['BOT_TOKEN'] || "FAKE"
 bot = Discordrb::Bot.new token: token, client_id: 280889496340398080
 
+puts "This bot's invite URL is #{bot.invite_url}."
+
 bot.message(content: 'what') do
   "That's what I thought"
 end
